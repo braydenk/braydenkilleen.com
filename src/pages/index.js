6e2f789cc87113cbe9b9fe910473a935d15b9dda
Brayden Killeen <braydenmkilleen@gmail.com>
2019-09-03 01:45:41 +1000

    Replaced defaults

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
 import React from "react"
+import styled from "styled-components"
 
-export default () => <div>Hello world!</div>
+const S = styled.div`
+  background-color: red;
+`
+
+export default () => <S>HELLO</S>
