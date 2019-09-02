70ba80b31c888362fd7b5c843cec453356ddffbd
Brayden Killeen <braydenmkilleen@gmail.com>
2019-09-03 04:57:47 +1000

    About page structure

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Layout from '../components/Layout';
 
-export default ({ data }) => {
+export default () => {
   return (
     <Layout>
-      <h1>About</h1>
+      <h1>About Me</h1>
+      <h2>Graduate Software Engineer at Eggion</h2>
+      <div>Currently working on - at work - spare time</div>
+      <div>Open-source projects - list of projects contributed to</div>
+      <div>Blog - info on the blog</div>
     </Layout>
   );
 };
