efb5949c473c56caafaaf2267728ff76ebf45a43
Brayden Killeen <braydenmkilleen@gmail.com>
2019-09-03 04:53:21 +1000

    Created projects page layout

---
+++ src/data/projects-data.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    id: 0,
+    icon: 'X',
+    title: 'Project 1',
+    path: '',
+    source: '',
+    description: '',
+  },
+  {
+    id: 1,
+    title: 'Project 2',
+    path: '',
+    source: '',
+    description: '',
+  },
+  {
+    id: 2,
+    title: 'Project 3',
+    path: '',
+    source: '',
+    description: '',
+  },
+  {
+    id: 3,
+    title: 'Project 4',
+    path: '',
+    source: '',
+    description: '',
+  },
+];
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
+import projectsData from '../data/projects-data';
+import styled from 'styled-components';
+import { Link } from 'gatsby';
 
-export default ({ data }) => {
+const ProjectsGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 2rem;
+  grid-row-gap: 2rem;
+`;
+
+const ProjectsTile = styled.div`
+  border: 1px solid black;
+`;
+
+export default () => {
   return (
     <Layout>
-      <h1>Projects</h1>
+      <ProjectsGrid>
+        {projectsData.map(project => {
+          return (
+            <ProjectsTile key={project.id}>
+              <div>
+                <div>Project Icon / Logo</div>
+                <h2>Project Title</h2>
+              </div>
+              <div>
+                <p>Project Description</p>
+                <Link to="/">Project Link</Link>
+              </div>
+            </ProjectsTile>
+          );
+        })}
+      </ProjectsGrid>
     </Layout>
   );
 };
